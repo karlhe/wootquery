e76ed3750aeb43fe6d5cf27a26336e4432886961
Karl He <karlhe@users.noreply.github.com>
2009-12-11 00:37:58 +0000

    jquery example/test, wootquery starter code

---
+++ jquery/jquery-input.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function(){
+    $("a").click(function(event){
+        $(this).append("!");
+        event.preventDefault();
+    });
+    $("#codify").click(function(event){
+        $(".code").toggleClass("highlight");
+        // $(this).replaceWith("Welcome to the Show!");
+    });
+});
