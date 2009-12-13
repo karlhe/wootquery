663adf58980b79d72d29a73a2c4f18383054f4b2
karl.he <karl.he@35b48e5e-d64a-11de-986c-23549013a0f8>
2009-12-13 05:27:06 +0000

    added html method

--- trunk/wootquery.js
+++ trunk/wootquery.js
@@ -1,19 +1,25 @@
 ﻿(function(){
     var _$ = window.$;
     var _wootQuery = window.wootQuery;
-    var wootQuery = window.$ = window.wootQuery = function(selector,context){
+    var wootQuery = window.$ = window.wootQuery = function(selector,context) {
         // Return a new wootQuery object
         return new wootQuery.fn.init(selector);
     }
     wootQuery.fn = wootQuery.prototype = {
         // Establish the state of the wootQuery instance
         init: function(selector) {
-            this.selector = selector || document;
+            this.dom = selector || document;
             return this;
         },
+        
         // Just for fun
         capture: function() {
             alert("All your base, are belong to us!");
+        },
+        
+        // Returns the innerHTML of the DOM element
+        html: function() {
+            return this.dom.innerHTML;
         },
     }
     // Allow access to wootQuery.prototype methods
