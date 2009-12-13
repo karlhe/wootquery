84fef8e5b9dd166485833e60e50d7e98bf690ad2
karl.he <karl.he@35b48e5e-d64a-11de-986c-23549013a0f8>
2009-12-13 05:16:41 +0000

    some kind of method calling possible

--- trunk/wootquery.js
+++ trunk/wootquery.js
@@ -2,6 +2,20 @@
     var _$ = window.$;
     var _wootQuery = window.wootQuery;
     var wootQuery = window.$ = window.wootQuery = function(selector,context){
-        alert("All your base are belong to us!");
+        // Return a new wootQuery object
+        return new wootQuery.fn.init(selector);
     }
+    wootQuery.fn = wootQuery.prototype = {
+        // Establish the state of the wootQuery instance
+        init: function(selector) {
+            this.selector = selector || document;
+            return this;
+        },
+        // Just for fun
+        capture: function() {
+            alert("All your base, are belong to us!");
+        },
+    }
+    // Allow access to wootQuery.prototype methods
+    wootQuery.fn.init.prototype = wootQuery.fn;
 })();
