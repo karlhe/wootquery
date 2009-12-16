373349c588a73e7db6f2267ef307ada5f4348bc7
karl.he <karl.he@35b48e5e-d64a-11de-986c-23549013a0f8>
2009-12-16 07:11:45 +0000

    added some comments on previous update

--- trunk/input.js
+++ trunk/input.js
@@ -1,11 +1,13 @@
+// This code should be executed when the DOM is ready
 $(document).ready(function() {
     $('p').append("ain't it?");
-    $('p').css('background','#fcc');
-    bg = $('p').css('background');
-    $('p').append(bg);
+    $('p').css('background','#fcc');    // Set BG color of <p> to #fcc
+    bg = $('p').css('background');      // Store current background color of <p> into variable bg
+    $('p').append(bg);                  
+    // This looks almost the same as normal CSS, but not quite. Note conventions.
     styles = {
         background: '#ccc',
         fontFamily: 'Consolas,"Courier New",Courier,monospace',
     }
-    $('div').css(styles);
+    $('div').css(styles);               // Merge the styles hash into the styles of <div>
 });
