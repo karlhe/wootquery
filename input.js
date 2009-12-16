efe1a0cded5df06d83729547ca4efa4f31720109
Karl He <karlhe@users.noreply.github.com>
2009-12-16 07:08:20 +0000

    implemented .css()

--- input.js
+++ input.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
     $('p').append("ain't it?");
+    $('p').css('background','#fcc');
+    bg = $('p').css('background');
+    $('p').append(bg);
+    styles = {
+        background: '#ccc',
+        fontFamily: 'Consolas,"Courier New",Courier,monospace',
+    }
+    $('div').css(styles);
 });
